7bea0a108815a7c13ef2f010c97efe8cc3bbb12a
omerozoglu <omerozoglu.1@gmail.com>
2020-02-01 15:24:57 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import {Routes,RouterModule} from '@angular/router'
-import { CommonModule } from '@angular/common';
-import { from } from 'rxjs';
-import { AddComponent } from './user/add/add.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { from } from "rxjs";
+import { AddComponent } from "./user/add/add.component";
+import { LoginComponent } from "./authentication/component/login/login.component";
 
 const routes: Routes = [
-  {path:'',redirectTo:'', pathMatch:'full'},
-  {path:'add',component:AddComponent}
+  { path: "", redirectTo: "", pathMatch: "full" },
+  { path: "add", component: AddComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports:[RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
